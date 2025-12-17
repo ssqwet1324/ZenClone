7726afe13124f538b70b113be04ac087c5ce6406
ssqwet1324 <sgerasimov407@gmail.com>
2025-12-17 11:08:31 +0300

    refactor: small changes

--- UsersService/go.mod
+++ UsersService/go.mod
@@ -8,13 +8,13 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/jackc/pgx/v5 v5.7.6
-	github.com/minio/minio-go/v7 v7.0.95
+	github.com/minio/minio-go/v7 v7.0.97
 	go.uber.org/zap v1.27.1
 	golang.org/x/crypto v0.46.0
 )
 
 require (
-	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/BurntSushi/toml v1.5.0 // indirect
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.14.2 // indirect
 	github.com/bytedance/sonic/loader v0.4.0 // indirect
@@ -33,11 +33,12 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/compress v1.18.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	github.com/klauspost/crc32 v1.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/minio/crc64nvme v1.0.2 // indirect
+	github.com/minio/crc64nvme v1.1.1 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -47,7 +48,7 @@ require (
 	github.com/quic-go/quic-go v0.57.1 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/rs/xid v1.6.0 // indirect
-	github.com/tinylib/msgp v1.3.0 // indirect
+	github.com/tinylib/msgp v1.6.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.1 // indirect
 	go.uber.org/mock v0.6.0 // indirect
---
+++ UsersService/internal/repository/minio.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	"UsersService/internal/entity"
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/minio/minio-go/v7"
+)
+
+// UploadAvatar - загружаем фото и сохраняем его имя в бд
+func (repo *PostgresRepository) UploadAvatar(ctx context.Context, userID uuid.UUID, bucketName string, avatarInfo entity.AvatarRequest) error {
+	// timestamp, чтобы ссылка менялась при каждой загрузке
+	objectName := fmt.Sprintf("%s/avatar_%d.jpg", userID.String(), time.Now().Unix())
+
+	_, err := repo.Client.PutObject(ctx, bucketName, objectName, avatarInfo.Reader, avatarInfo.Size, minio.PutObjectOptions{
+		ContentType: "image/jpg",
+	})
+	if err != nil {
+		return fmt.Errorf("UploadAvatar: error uploading avatar: %w", err)
+	}
+
+	// Сохраняем путь к аватару в БД
+	_, err = repo.DB.Exec(ctx, `UPDATE users SET avatar_url = $1 WHERE id = $2`, objectName, userID)
+	if err != nil {
+		return fmt.Errorf("UploadAvatar: error saving avatar_url in DB: %w", err)
+	}
+
+	return nil
+}
+
+// GetAvatarURL - получаем url аватарки по его имени
+func (repo *PostgresRepository) GetAvatarURL(ctx context.Context, bucketName string, userID uuid.UUID) (string, error) {
+	var objectName string
+	err := repo.DB.QueryRow(ctx, `SELECT avatar_url FROM users WHERE id = $1`, userID).Scan(&objectName)
+	if err != nil {
+		return "", fmt.Errorf("GetAvatarURL: error fetching avatar_url from DB: %w", err)
+	}
+
+	publicEndpoint := repo.Config.MinIoPublicEndpoint
+	if publicEndpoint == "" {
+		publicEndpoint = repo.Config.MinioEndpoint
+	}
+
+	if objectName == "default" {
+		// Возвращаем дефолтную аватарку
+		avatarURL := fmt.Sprintf("%s/%s/%s", publicEndpoint, bucketName, defaultAvatar)
+		return avatarURL, nil
+	}
+
+	avatarURL := fmt.Sprintf("%s/%s/%s", publicEndpoint, bucketName, objectName)
+	return avatarURL, nil
+}
